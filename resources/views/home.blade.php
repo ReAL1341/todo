94d978437d37f512df489868a2343d0389a02643
TAJIMA_YUHEI <azidahakaskritmk@gmail.com>
2022-03-12 11:08:21 +0900

    feat:Add basic html of todo input space

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ToDo</title>
+</head>
+<body>
+
+
+    <!--add_todo_section -->
+    <section class="add_todo_section">
+
+        <div><span>+</span>ToDo</div>
+
+        <form method="post">
+            <input type="text" id="todo_content" name="todo_content">
+            <input type="datetime-local" id="deadline" name="deadline">
+            <input type="submit" id="submit" value="+">
+        </form>
+
+    </section>
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('/todo')->group(function(){
+    Route::get('/','HomeController@show');
+});
