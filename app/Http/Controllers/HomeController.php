d51a269b4dfd4c41560797da2fa1ff287ea7711c
TAJIMA_YUHEI <azidahakaskritmk@gmail.com>
2022-03-14 21:28:32 +0900

    feat:Add method to update record

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,7 +21,7 @@ class HomeController extends Controller
     public function store(Request $request){
    
         //+ボタンの処理
-        if($request->has('send')){
+        if($request->has('add')){
             //保存処理
             $todo = new Todo();
             $form = $request->all();
@@ -37,6 +37,29 @@ class HomeController extends Controller
         if($request->has('delete')){
             //レコード削除処理
             DB::table('todo')->where('id',$request->delete)->delete();
+            
+            $items = DB::table('todo')->get();
+            return redirect('/todo')->with(compact('items'));
+        }
+
+        //編集ボタンの処理
+        if($request->has('edit')){
+            $items = DB::table('todo')->get();
+            foreach($items as $item){
+                if($item->id == $request->edit){
+                    $item->mode = 'edit';
+                }
+            };
+            return view('home',compact('items'));
+        }
+
+        //編集完了ボタンの処理
+        if($request->has('update')){
+            $param = [
+                'todo_content' => $request->todo_content,
+                'deadline' => $request->deadline,
+            ];
+            DB::table('todo')->where('id',$request->update)->update($param);
             
             $items = DB::table('todo')->get();
             return redirect('/todo')->with(compact('items'));
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('/css/home.css')}}">
+    <script src="{{asset('/js/home.js')}}"></script>
     <title>ToDo</title>
 </head>
 <body>
@@ -20,7 +21,7 @@
             @csrf
                 <p><input type="text" id="todo_content" name="todo_content"></p>
                 <input type="datetime-local" id="deadline" name="deadline">
-                <button type="submit"id="submit" name="send" value="+">+</button>
+                <button type="submit"id="submit" name="add" value="+">+</button>
             </form>
         </p>
 
@@ -32,11 +33,20 @@
 
         @foreach ($items as $item)
             <p>
-                <form method="post">
+                <form method="post"><!--一括削除-->
                 @csrf
-                    <span>{{ $item->todo_content }}</span>
-                    <span>{{ $item->deadline }}</span>
-                    <button type="submit" formaction="" name="delete" value="{{ $item->id }}">×</button>
+
+                @if(property_exists($item,'mode'))
+                    <input type="text" id="todo_content" name="todo_content" value="{{$item->todo_content}}"><!--未完-->
+                    <input type="datetime-local" id="deadline" name="deadline" value="{{$item->deadline}}"><!--未完-->
+                    <button type="submit"id="submit" name="update" value="{{ $item->id }}">完了</button><!--未完-->
+                    <!--やめるボタン-->
+                @else
+                    <span>{{$item->todo_content}}</span>
+                    <span>{{$item->deadline}}</span>
+                    <button type="submit" formaction="" name="edit" value="{{$item->id}}">編集</button>
+                    <button type="submit" formaction="" name="delete" value="{{$item->id}}">×</button><!--未完-->
+                @endif
                 </form>
             </p>
         @endforeach
